905905ffef8b020e4ac8fcf862e45fb8dc0f9aef
chebykin1993 <chebykin1993@list.ru>
2023-06-19 14:29:16 +0300

    feat: complete src/fs/copy.js, create.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
-const copy = async () => {
-    // Write your code here 
+import fs from "fs";
+import path from "path";
+
+const copy = async (src, dest) => {
+  const exist = fs.existsSync(src);
+  const stats = exist && fs.statSync(src);
+  const isDirectory = stats && stats.isDirectory();
+
+  if (isDirectory) {
+    if (fs.existsSync(dest)) {
+      throw Error("FS operation failed");
+    }
+    if (!fs.existsSync(dest)) fs.mkdirSync(dest);
+
+    fs.readdirSync(src).forEach((childItemName) => {
+      copy(path.join(src, childItemName), path.join(dest, childItemName));
+    });
+  } else {
+    if (!fs.existsSync(dest)) fs.copyFileSync(src, dest);
+  }
+
+  if (!isDirectory) {
+    throw Error("FS operation failed");
+  }
 };
 
-await copy();
+await copy("./src/fs/files", "./src/fs/files_copy");
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import fs, { existsSync } from "fs";
+
 const create = async () => {
-    // Write your code here 
+  if (!fs.existsSync("./src/fs/fresh.txt")) {
+    fs.writeFile("./src/fs/fresh.txt", "I am fresh and young", (err) => {
+      if (err) throw err;
+    });
+  } else {
+    throw Error("FS operation failed");
+  }
 };
 
 await create();
